1308b18741ea9e778075ff262c670bbe4084f207
Vanny242 <167153032+Vanny242@users.noreply.github.com>
2025-04-08 09:38:09 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>      
+    <body> 
+        <h1>My First PHP </h1>
+       <h2>
+        <?php
+            echo "Hello World";
+        ?>
+        </h2>
+    </body>
+</html>
