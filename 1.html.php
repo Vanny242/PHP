6b932617ac19291e72f08566038f3d00766991c1
Vanny242 <167153032+Vanny242@users.noreply.github.com>
2025-04-08 09:50:30 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -2,10 +2,10 @@
     <head>
     </head>      
     <body> 
-        <h1>My First PHP </h1>
-       <h2>
+        <h1>My First PHP </h1>    #page title
+       <h2> 
         <?php
-            echo "Hello World";
+            echo "Hello World"; #print output
         ?>
         </h2>
     </body>
