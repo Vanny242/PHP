5096deebe6294163a4d84b99089908ea923adfdb
Vanny242 <167153032+Vanny242@users.noreply.github.com>
2025-04-08 11:35:58 +0800

    Create 12.logout.php

---
+++ 12.logout.php
@@ -0,0 +1,8 @@
+#Delete the login session and redirect the user back to the login page
+<?php
+    session_start();
+    unset($_SESSION["id"]);
+    echo "Logout successful....";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+
+?>
