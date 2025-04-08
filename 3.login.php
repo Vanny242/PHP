9b7cd9129e613abfbc560ba9c51839f227843d82
Vanny242 <167153032+Vanny242@users.noreply.github.com>
2025-04-08 10:17:54 +0800

    Create 3.login.php

---
+++ 3.login.php
@@ -0,0 +1,6 @@
+<?php 
+    if (($_POST["id"] == "Vanny24") && ($_POST["pwd"]=="Vanny242"))  #set username and pwd 
+        echo"Login successful";    #will display this if true
+    else
+        echo "Login failed";    #will display this if false
+?>
