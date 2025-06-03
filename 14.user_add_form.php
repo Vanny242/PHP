49c7e1b4a997a739fe8e288625d97fb41a55ab06
Vanny242 <167153032+Vanny242@users.noreply.github.com>
2025-06-03 10:22:41 +0800

    14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -3,12 +3,15 @@
     <head><title>Add new user</title></head>
     <body>
 <?php        
+#Disable Error Reporting and Start Session
     error_reporting(0);
     session_start();
+#Check if User is Logged in 
     if (!$_SESSION["id"]) {
         echo "Please log in to your account";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
+#if Logged in: Show User Creation Form
     else{    
         echo "
             <form action=15.user_add.php method=post>
